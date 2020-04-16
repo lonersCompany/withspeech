b38186be3ae45a38a159d5f85d4836e2c8930c0b
Schittaci-air <barak.simon.as@gmail.com>
2020-04-16 15:33:58 +0200

    lol fix

--- src/components/AuthLayer.jsx
+++ src/components/AuthLayer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import SingUpForm from "./necojineho";
+import SingUpForm from "./SingUpFormM";
 import SingInForm from "./SingInForm";
 
 const LogInPage = ({ children }) => {
